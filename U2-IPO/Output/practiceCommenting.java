66ab73ca998b089e1316d036839d7d3b81302108
Mrs. Catherine McCaffery <42382779+MrsMcCaffery@users.noreply.github.com>
2019-02-26 14:37:49 -0500

    practiceCommenting complete

---
+++ U2-IPO/Output/practiceCommenting.java
@@ -0,0 +1,23 @@
+/*
+Name: Catherine McCaffery
+Course: ICS 3U
+Teacher: Mrs. McCaffery
+Task: Practice Commenting
+
+Description: Prints a name and the square of the number
+*/
+
+public class practiceCommenting
+{
+  public static void main (String[] args)
+  {
+    //define variables for number,name, and squared
+    int number = 7;
+    String name = "Amy";
+    int squared = number*number;
+    
+    //Print information for user
+    System.out.println ("Hi "+ name + "! \nIf your favourite number is " + number + " then the square of that number is " + squared + ".");
+    
+  }//end method main
+}//end class practiceCommenting
