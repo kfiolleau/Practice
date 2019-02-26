a13f33427b52a1eebe08c353a7141f8a315ef4a7
Mrs. Catherine McCaffery <42382779+MrsMcCaffery@users.noreply.github.com>
2019-02-26 15:15:18 -0500

    outputAssignment completed

---
+++ U2-IPO/Output/outputAssignment.java
@@ -0,0 +1,29 @@
+public class OutputAssignment {
+  
+  public static void main(String[] args) { 
+    
+    // Prints the first line from my favourite quote or song.
+    System.out.println("1) favourite song first line");
+    System.out.println("Some say love, it is a river ...");
+    
+    // Print the first line from my favourite song, with each word on a separate line.
+    System.out.println("\n2) favourite song first line, all words separate lines");
+    System.out.println("Some\nsay\nlove,\nit\nis\na\nriver\n...");
+    
+    // Print each of the following designs below. 
+    System.out.println("\n3) Shapes");
+    System.out.println("\ta) Parallelogram");
+    System.out.println("\t\t\t   PARALLELOG\n\t\t\t  A        R\n\t\t\t R       A\n\t\t\tALLELOGRAM\n");
+    System.out.println("\tb) Trapezoid");
+    System.out.println("\t\t\t  APEZO\n\t\t\t R     I\n\t\t\tTRAPEZOID\n");
+    System.out.println("\tc) Rectangle");
+    System.out.println("\t\t\tRECTANGLE\n\t\t\tE       L\n\t\t\tC       G\n\t\t\tT       N\n\t\t\tA       A\n\t\t\tN       T\n\t\t\tG       C\n\t\t\tL       E\n\t\t\tELGNATCER\n");
+    System.out.println("\td) Kite");
+    System.out.println("\t\t\t   D\n\t\t\t  III\n\t\t\t AAAAA\n\t\t\tMMMMMMM\n\t\t\t OOOOO\n\t\t\t  NNN\n\t\t\t   D");
+    
+    // Print my initials as 7x7 blocks of the asterisk character (*)
+    System.out.println("\n4) Initials");
+    System.out.println("\t\t\t  ***  *     *\n\t\t\t *   * **   **\n\t\t\t*      * * * *\n\t\t\t*      *  *  *\n\t\t\t*      *     *\n\t\t\t *   * *     *\n\t\t\t  ***  *     *");
+
+}// end method main 
+}// end class outputAssignment
