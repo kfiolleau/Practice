48409771bf2aefad354aed5e689e1a88cf49bfd1
Mrs. Catherine McCaffery <42382779+MrsMcCaffery@users.noreply.github.com>
2019-02-26 15:27:13 -0500

    Create task1.java

---
+++ U2-IPO/Output/task1.java
@@ -0,0 +1,15 @@
+public class task1 {
+  
+  public static void main(String[] args) {
+  
+    // part a
+    System.out.println("My favourite book is \"Green Eggs and Ham\".");
+    
+    // part b
+    System.out.println("What sport do you like?\nI like soccer\\football.");
+    
+    // part c
+    System.out.println("My\n\tname\n\t\tis\n\t\t\tTab");
+
+  }// end method main
+}// end class task1
